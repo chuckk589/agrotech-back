56e65f9872fa30fe9539a52d62a7a21fbac5565c
andrew <chuckk589@gmail.com>
2025-04-20 13:12:37 +0300

    feat: assets cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 import ORMOptionsProvider from './configs/mikro-orm.config';
 import { DatabaseModule } from './database/database.module';
 import { LicensingModule } from './licensing/licensing.module';
 import { ManualsModule } from './manuals/manuals.module';
-import { SimulatorsModule } from './simulators/simulators.module';
-import { VersionsModule } from './versions/versions.module';
 import { NewsModule } from './news/news.module';
+import { SimulatorsModule } from './simulators/simulators.module';
 import { TicketsModule } from './tickets/tickets.module';
-import { ConfigModule } from './config/config.module';
+import { VersionsModule } from './versions/versions.module';
 
 @Module({
   imports: [
@@ -22,7 +22,14 @@ import { ConfigModule } from './config/config.module';
     DatabaseModule,
     LicensingModule,
     MikroOrmModule.forRoot(ORMOptionsProvider),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', '/public/'), serveRoot: '/static' }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '/public/'),
+      serveRoot: '/static',
+      serveStaticOptions: {
+        immutable: true,
+        maxAge: '1y',
+      },
+    }),
     NewsModule,
     TicketsModule,
     ConfigModule,
